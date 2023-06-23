768d070366a52ef281b08d3f48397937a34ed35e
fractal161 <628snapdragon@gmail.com>
2023-06-23 13:41:48 -0500

    make cell selector a floating div

--- src/routes/Sheet.svelte
+++ src/routes/Sheet.svelte
@@ -2,49 +2,76 @@
   Contains a table and supports the following features:
     - one cell is able to be marked as unselected
     - colors of each cell can be set (to show differing files)
-  TODO: currently selection logic is super inefficient because it literally checks all
-  cells instead of just the one or two that matter.
 -->
 
 <script lang="ts">
   export let headers: Array<string>;
   export let entries: Array<Array<string>>;
+  let cells: Array<Array<HTMLTableCellElement>> = Array.from(
+    Array(entries.length+1), () => []
+  );
   export let selected: [number, number] | undefined = undefined;
-  const makeCellSelected = async (i: number, j: number) => {
+  let selector: {
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+  } = {
+    top: 0,
+    left: 0,
+    width: 0,
+    height: 0,
+  };
+  let selectorStyle: string;
+  $: selector, selectorStyle = `left: ${selector.left}px;
+    top: ${selector.top}px;
+    width: ${selector.width}px;
+    height: ${selector.height}px`;
+  const updateSelector = (i: number, j: number): void => {
     if (selected !== undefined && selected[0] === i && selected[1] === j) {
       selected = undefined;
     }
     else {
       selected = [i, j];
+      let cell = cells[i][j];
+      console.assert(cell !== undefined);
+      selector.top = cell.offsetTop-1;
+      selector.left = cell.offsetLeft-1;
+      selector.width = cell.offsetWidth-2;
+      selector.height = cell.offsetHeight-2;
     }
-  }
-  const isSelected = (i: number, j: number): boolean => {
-    return selected !== undefined && i === selected[0] && j === selected[1];
   }
 </script>
 
-<table>
-  <thead>
-    <tr>
-      {#each headers as header, j}
-        <th
-          class:selected={selected && isSelected(-1, j)}
-          on:click={() => makeCellSelected(-1, j)}
-        >{header}</th>
-      {/each}
-    </tr>
-  </thead>
-  {#each entries as row, i}
-    <tr>
-      {#each row as entry, j}
-        <td
-          class:selected={selected && isSelected(i, j)}
-          on:click={() => makeCellSelected(i, j)}
-        >{entry}</td>
-      {/each}
-    </tr>
-  {/each}
-</table>
+<div class="wrapper">
+  <div
+    class={selected === undefined ? "hidden" : "selector"}
+    style={selectorStyle}
+  >
+  </div>
+  <table>
+    <thead>
+      <tr>
+        {#each headers as header, j}
+          <th
+            bind:this={cells[0][j]}
+            on:click={() => updateSelector(0, j)}
+          >{header}</th>
+        {/each}
+      </tr>
+    </thead>
+    {#each entries as row, i}
+      <tr>
+        {#each row as entry, j}
+          <td
+            bind:this={cells[i+1][j]}
+            on:click={() => updateSelector(i+1, j)}
+          >{entry}</td>
+        {/each}
+      </tr>
+    {/each}
+  </table>
+</div>
 
 <style>
   table {
@@ -54,7 +81,17 @@
     border: 1px solid gray;
     user-select: none;
   }
-  .selected {
+  .wrapper {
+    position: relative;
+  }
+  .hidden {
+    position: absolute;
+    display: none;
+    pointer-events: none;
+  }
+  .selector {
+    position: absolute;
     border: 2px solid blue;
+    pointer-events: none;
   }
 </style>
